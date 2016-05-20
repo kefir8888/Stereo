54d673a48f1ec4f1ef756d87286a763c20c26182
kefir8888 <kefir8888@gmail.com>
2016-05-20 14:13:01 +0300

    Fixed algorythm. First well result.

--- stereo_mod.cpp
+++ stereo_mod.cpp
@@ -1,12 +1,14 @@
 # include <SFML/Graphics.hpp>
+#include "assert.h"
+#include "math.h"
 
-const int PICT_X = 280;//505;//1002; //960; //280;
-const int PICT_Y = 210;//621;//900;  //658; //210;
+int PICT_X = 0;//505; //505;//1002; //960; //280;
+int PICT_Y = 0;//621; //621;//900;  //658; //210;
 
 const int SPACE = 10; //Space between pictures.
 
-const int WIND_X = PICT_X * 2 + 3 * SPACE; //2 * PICT_X + 3 * SPACE;
-const int WIND_Y = PICT_Y + 2 * SPACE; //2 * PICT_Y + 3 * SPACE;
+int WIND_X = PICT_X * 2 + 3 * SPACE; //2 * PICT_X + 3 * SPACE;
+int WIND_Y = PICT_Y + 2 * SPACE; //2 * PICT_Y + 3 * SPACE;
 
 const int BLOCK_SIZE = 10;
 
@@ -26,7 +28,7 @@ void calc_res (Image& left, Image& result, Image& depth, double ang)
 	for (int i = 0; i < PICT_X; i ++)
 		for (int j = 0; j < PICT_Y; j ++)
 			{
-			int x = i + ang * depth.getPixel (i, j).r;
+			int x = i + ang * (depth.getPixel (i, j).r - 128);
 			
 			if (x > PICT_X || x < 0) continue;
 			
@@ -34,49 +36,66 @@ void calc_res (Image& left, Image& result, Image& depth, double ang)
 			}
 	}
 
+int bound (int inp, int min, int max)
+	{
+	if (inp < min) return min;
+	if (inp > max) return max;
+	
+	return inp;
+	}
+
 void calculate_depths (Image& left, Image& right, Image& depth)
 	{
-	/*for (int i = 0; i < 20; i ++)
-		for (int j = 0; j < 30; j ++)
-			depth.setPixel (i, j, Color (100, 100, 100));
-	*/
+	//for (int i = MAX_BLOCK_SHIFT + BLOCK_SIZE  / 2; i < PICT_X - MAX_BLOCK_SHIFT - BLOCK_SIZE  / 2; i ++)
+	//	{
+	//	for (int j = MAX_BLOCK_SHIFT + BLOCK_SIZE / 2; j < PICT_Y - MAX_BLOCK_SHIFT - BLOCK_SIZE / 2; j ++)
 	
-	for (int i = MAX_BLOCK_SHIFT + BLOCK_SIZE  /2; i < PICT_X - MAX_BLOCK_SHIFT - BLOCK_SIZE  /2; i ++)
+	for (int i = 1; i < PICT_X - 1; i ++)
 		{
-		for (int j = MAX_BLOCK_SHIFT + BLOCK_SIZE / 2; j < PICT_Y - MAX_BLOCK_SHIFT - BLOCK_SIZE / 2; j ++)
+		for (int j = 1; j < PICT_Y - 1; j ++)
 			{
 			int shift = - MAX_BLOCK_SHIFT;
-			int cur_minimal_sum = 1000000;
+			double cur_avg_min_sum = 1000000;
 			
 			for (int k = - MAX_BLOCK_SHIFT; k < MAX_BLOCK_SHIFT; k ++)
 				{
-				int cur_sum = 0;
+				double avg_sum = 0;
+				
+				int dropped = 0;
 				
 				for (int l = - BLOCK_SIZE / 2; l < BLOCK_SIZE / 2; l ++)
 					for (int m = - BLOCK_SIZE / 2; m < BLOCK_SIZE / 2; m ++)
 						{
+						if (i + l < 0 || j + m < 0 || i + k + l < 0 ||
+						    i + l > WIND_X || j + m > WIND_Y || i + k + l > WIND_X)
+							{
+							dropped ++;
+							continue;
+							}
+						
 						Color le = left.getPixel (i + l, j + m);
 						Color ri = right.getPixel (i + k + l, j + m);
-						cur_sum += abs (le.r - ri.r) + abs (le.g - ri.g) + abs (le.b - ri.b);
+						avg_sum += abs (le.r - ri.r) + abs (le.g - ri.g) + abs (le.b - ri.b);
 						}
 				
-				if (cur_sum < cur_minimal_sum)
+				avg_sum *= pow (BLOCK_SIZE, 2) / (pow (BLOCK_SIZE, 2) - dropped);
+				
+				if (avg_sum < cur_avg_min_sum)
 					{
 					shift = k;
-					cur_minimal_sum = cur_sum;
+					cur_avg_min_sum = avg_sum;
 					}
 				}
 			
-			depth.setPixel (i, j, Color (10 * shift, 10 * shift, 10 * shift));
-			//depth.setPixel (i, j, Color (128 + 10 * shift, 128 + 10 * shift, 128 + 10 * shift));
+			depth.setPixel (i, j, Color (128 + shift, 128 + shift, 128 + shift));
 			}
 		}
 	}
 
 void smooth_depth (Image& depth)
 	{
-	for (int i = 3; i < WIND_X - 3; i ++)
-		for (int j = 3; j < WIND_Y - 3; j ++)
+	for (int i = 3; i < PICT_X - 3; i ++)
+		for (int j = 3; j < PICT_Y - 3; j ++)
 			{
 			int sum = 0;
 			
@@ -90,28 +109,78 @@ void smooth_depth (Image& depth)
 			}
 	}
 
-int main()
+void sub_mid_depth (Image& depth)
 	{
-	RenderWindow App (sf::VideoMode (WIND_X, WIND_Y, 32), "Elijah stereo _demo.");
+	int min =  10000;
+	int max = -10000;
+	
+	for (int i = 0; i < PICT_X; i ++)
+		for (int j = 0; j < PICT_Y; j ++)
+			{
+			Color col = depth.getPixel (i, j);
+			
+			if (col.r > max) max = col.r;
+			if (col.r < min) min = col.r;
+			}
+	
+	int mid = (max + min) / 3;
+	
+	for (int i = 0; i < PICT_X; i ++)
+		for (int j = 0; j < PICT_Y; j ++)
+			{
+			Color col = depth.getPixel (i, j);
+			int newcol = col.r - mid;
+			depth.setPixel (i, j, Color (newcol, newcol, newcol));
+			}
+	}
+
+int main (int argc, char** argv)
+	{
+	if (argc < 2)
+		{
+		fprintf (stderr, "Give stereopare .png filename.\n");
+		assert (false);
+		}
+	
+	Image temp;
 	Image left;
 	Image right;
 	Image depth;
 	Image result;
-	//Image& left
 	Texture output_texture;
 	
-	//const Image& im = pict;
+	if (!temp.loadFromFile  (argv [1])) return -1;
 	
-	if (!left.loadFromFile  ("left.png")) return -1;
-	if (!right.loadFromFile ("right.png")) return -1;
+	Vector2u sz = temp.getSize ();
+	
+	PICT_X = sz.x / 2;
+	PICT_Y = sz.y;
+	
+	WIND_X = PICT_X * 2 + 3 * SPACE; //2 * PICT_X + 3 * SPACE;
+	WIND_Y = PICT_Y + 2 * SPACE; //2 * PICT_Y + 3 * SPACE;
+	
+	RenderWindow App (sf::VideoMode (WIND_X, WIND_Y, 32), "Elijah stereo _demo.");
+	
+	//if (!left.loadFromFile  ("girl_l.png")) return -1;
+	//if (!right.loadFromFile ("girl_r.png")) return -1;
+	left.create   (sz.x / 2, sz.y, Color (30, 60, 120));
+	right.create  (sz.x / 2, sz.y, Color (60, 120, 30));
 	depth.create  (PICT_X, PICT_Y, Color (200, 100, 100));
 	result.create (PICT_X, PICT_Y, Color (100, 200, 100));
+	
+	for (int i = 0; i < sz.x / 2; i ++)
+		for (int j = 0; j < sz.y; j ++)
+			{
+			left.setPixel  (i, j, temp.getPixel (i, j));
+			right.setPixel (i, j, temp.getPixel (i + sz.x / 2, j));
+			}
 	
 	double ang        = 0.01;
 	bool   to_refresh = true;
 	
 	calculate_depths (left, right, depth);
 	smooth_depth (depth);
+	//sub_mid_depth (depth);
 	
 	while (App.isOpen())
 		{
